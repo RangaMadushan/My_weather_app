312ba968b8f89bc91084c796576a33800a2df8ea
Ranga Madushan <rangamadushan@Rangas-MacBook-Air.local>
2018-03-28 23:34:39 +0530

    before networking part, only table view and navigation control.

---
+++ MyWeatherApp/VC/WeatherTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherTableViewController.swift
+//  MyWeatherApp
+//
+//  Created by Ranga Madushan on 3/28/18.
+//  Copyright © 2018 Ranga Madushan. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+   
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+
+}//class
